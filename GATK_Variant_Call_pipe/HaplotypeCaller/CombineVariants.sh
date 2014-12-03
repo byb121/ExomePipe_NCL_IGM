0f9a23d36e44d2f667eae08e1a6e9e05764e16d3
byb121 <thelastsheep@hotmail.com>
2014-12-03 11:51:33 +0000

    Scala script are no longer needed in GATK pipeline

---
+++ GATK_Variant_Call_pipe/HaplotypeCaller/CombineVariants.sh
@@ -0,0 +1,74 @@
+#! /bin/bash
+#$ -cwd -V -j y
+#$ -l h_vmem=16g
+# #$ -M yaobo.xu@ncl.ac.uk
+# #$ -m e
+
+set -e
+res1=$(date +%s.%N) # use to calculate whole run time of the job
+
+#/********************** on lampredi2
+#module load apps/gatk/3.1.1/noarch
+#module load apps/gatkqueue/3.1.1/noarch
+#**********************/
+
+
+#/********************** on fmscluster
+module add apps/java/jre-1.7.0_55
+module add apps/gatk/3.2-protected
+SCRATCH_DIR=$TMPDIR
+export _JAVA_OPTIONS="-XX:-UseLargePages"
+GATKDIR="$GATK_ROOT"
+#**********************/
+
+REF_FILE=$1
+INPUT_FOLDER=$2
+OUTPUT_FILE=$3
+JOB_ID=$4
+JAVA_TMP_DIR_NAME=$5
+
+echo "Java temp dir name: $JAVA_TMP_DIR_NAME"
+JAVA_TEMP_DIR="$TMPDIR/GenotypeGVCFs_${JOB_ID}${JAVA_TMP_DIR_NAME}"
+if [ ! -d $JAVA_TEMP_DIR ]; then
+        echo mkdir $JAVA_TEMP_DIR
+        mkdir $JAVA_TEMP_DIR
+else
+        echo "Warning: Temprorary directory $JAVA_TEMP_DIR exists!"
+fi
+
+
+if [ ! -d $INPUT_FOLDER ]; then
+	exit "Input folder: $INPUT_FOLDER does not exist"
+fi
+
+INPUT=""
+VCFs=($INPUT_FOLDER/*.vcf)
+
+if [ ${#VCFs[@]} -eq 1 ]; then
+	exit "No VCF files folder in the folder!"
+fi
+
+for VCF in "${VCFs[@]}"; do
+	VCF=(`echo -n $VCF`)
+	INPUT="$INPUT -V $VCF"
+done
+
+java -Djava.io.tmpdir=$JAVA_TEMP_DIR -Xmx12g -jar $GATKDIR/GenomeAnalysisTK.jar -T CombineVariants -R $REF_FILE -o $OUTPUT_FILE --assumeIdenticalSamples \
+$INPUT
+
+#rm -r $JAVA_TEMP_DIR
+
+echo $'\n'"["`date`"]: The whole job is DONE!!"
+
+# runtime calculation
+res2=$(date +%s.%N)
+dt=$(echo "$res2 - $res1" | bc)
+dd=$(echo "$dt/86400" | bc)
+dt2=$(echo "$dt-86400*$dd" | bc)
+dh=$(echo "$dt2/3600" | bc)
+dt3=$(echo "$dt2-3600*$dh" | bc)
+dm=$(echo "$dt3/60" | bc)
+ds=$(echo "$dt3-60*$dm" | bc)
+echo "----------------------------- hua li li de fen jie xian -----------------------------"
+printf "Total runtime: %d:%02d:%02d:%02.4f\n" $dd $dh $dm $ds
+echo "exit status $?"
--- GATK_Variant_Call_pipe/HaplotypeCaller/GATK_Recalibration_b37_31JUL2014.sh
+++ GATK_Variant_Call_pipe/HaplotypeCaller/GATK_Recalibration_b37_31JUL2014.sh
@@ -2,9 +2,12 @@
 #$ -cwd
 #$ -j y
 #$ -pe smp 4
-# #$ -l mem_free=40G
-# $ -j h="node1[7-9]|node20"
+#$ -l h_vmem=16G
 
+# MQ is dropped to use temporarily. It is not informative for the recalibration because the use of bwa-mem
+# Majority (must be over 99%) of the scores is 60
+
+set -e
 # Load Modules
 #/******************** on lampredi2
 #module load apps/R/3.0.0/gcc-4.4.6+lapack-3.4.1+blas-1
@@ -13,22 +16,24 @@
 #********************/
 
 #/******************** on fmscluster
-module load apps/java
-export _JAVA_OPTIONS="-XX:-UseLargePages -Xms4096m"
-GATKDIR="/home/a5907529/WORKING_DATA/QC_GOOD/GATK3"
-REF_DIR="/sharedlustre/IGM/bundle2.8/hg19"
+module add apps/java/jre-1.7.0_55
+module add apps/gatk/3.2-protected
+SCRATCH_DIR=$TMPDIR
+export _JAVA_OPTIONS="-XX:-UseLargePages"
+GATKDIR="$GATK_ROOT"
+REF_DIR="/sharedlustre/IGM/GATK/bundle2.8/b37"
 #********************/
 
 SAMPLE_PATH=`pwd`
-OUTPUT_VCF_PREFIX="New_HaplotypeCaller"
-RAW_VCF_INPUT="$SAMPLE_PATH/HaplotypeCaller.vcf"
+OUTPUT_VCF_PREFIX="New_HC_out"
+RAW_VCF_INPUT="$SAMPLE_PATH/HC_out_combined.vcf"
 
-REF_FILE="${REF_DIR}/ucsc.hg19.fasta"
-HAPMAP="${REF_DIR}/hapmap_3.3.hg19.vcf"
-DBSNP="${REF_DIR}/dbsnp_138.hg19.vcf"
-OMNI="${REF_DIR}/1000G_omni2.5.hg19.vcf"
-MILLS_INDEL="${REF_DIR}/Mills_and_1000G_gold_standard.indels.hg19.vcf"
-SNP_1000G="${REF_DIR}/1000G_phase1.snps.high_confidence.hg19.vcf"
+REF_FILE="${REF_DIR}/human_g1k_v37_decoy.fasta"
+HAPMAP="${REF_DIR}/hapmap_3.3.b37.vcf"
+DBSNP="${REF_DIR}/dbsnp_138.b37.vcf"
+OMNI="${REF_DIR}/1000G_omni2.5.b37.vcf"
+MILLS_INDEL="${REF_DIR}/Mills_and_1000G_gold_standard.indels.b37.vcf"
+SNP_1000G="${REF_DIR}/1000G_phase1.snps.high_confidence.b37.vcf"
 
 SNP_RECAL_FILE="${RAW_VCF_INPUT}.SNP.recali"
 SNP_TRANCH_FILE="${RAW_VCF_INPUT}.SNP.tranches"
@@ -44,13 +49,13 @@ echo "Start to recalibrate variants is file:"
 echo "$RAW_VCF_INPUT"
 
 # SNP error model
-java -Xmx4g -jar $GATKDIR/GenomeAnalysisTK.jar -T VariantRecalibrator -R $REF_FILE \
+java -Xmx8g -jar $GATKDIR/GenomeAnalysisTK.jar -T VariantRecalibrator -R $REF_FILE \
+--disable_auto_index_creation_and_locking_when_reading_rods \
 -resource:hapmap,known=false,training=true,truth=true,prior=15.0 $HAPMAP \
 -resource:dbsnp,known=true,training=false,truth=false,prior=2.0 ${DBSNP} \
 -resource:omni,known=false,training=true,truth=true,prior=12.0 $OMNI \
 -resource:1000G,known=false,training=true,truth=false,prior=10.0 ${SNP_1000G} \
 -an QD \
--an MQ \
 -an MQRankSum \
 -an ReadPosRankSum \
 -an FS \
@@ -60,8 +65,10 @@ java -Xmx4g -jar $GATKDIR/GenomeAnalysisTK.jar -T VariantRecalibrator -R $REF_FI
 -recalFile $SNP_RECAL_FILE \
 -tranchesFile $SNP_TRANCH_FILE \
 -rscriptFile $SNP_RECAL_R_SCRIPT
+#-an MQ \
 
-java -Xmx4g -jar $GATKDIR/GenomeAnalysisTK.jar -T ApplyRecalibration -R $REF_FILE \
+java -Xmx8g -jar $GATKDIR/GenomeAnalysisTK.jar -T ApplyRecalibration -R $REF_FILE \
+--disable_auto_index_creation_and_locking_when_reading_rods \
 -input ${RAW_VCF_INPUT} \
 --ts_filter_level 99.5 \
 -recalFile $SNP_RECAL_FILE \
@@ -70,7 +77,8 @@ java -Xmx4g -jar $GATKDIR/GenomeAnalysisTK.jar -T ApplyRecalibration -R $REF_FIL
 -o $SNP_RECALI_OUTPUT
 
 #indel model
-java -Xmx4g -jar $GATKDIR/GenomeAnalysisTK.jar -T VariantRecalibrator -R $REF_FILE \
+java -Xmx8g -jar $GATKDIR/GenomeAnalysisTK.jar -T VariantRecalibrator -R $REF_FILE \
+--disable_auto_index_creation_and_locking_when_reading_rods \
 --maxGaussians 4 \
 -resource:mills,known=false,training=true,truth=true,prior=12.0 $MILLS_INDEL \
 -resource:dbsnp,known=true,training=false,truth=false,prior=2.0 ${DBSNP} \
@@ -85,10 +93,12 @@ java -Xmx4g -jar $GATKDIR/GenomeAnalysisTK.jar -T VariantRecalibrator -R $REF_FI
 -tranchesFile $INDEL_TRANCH_FILE \
 -rscriptFile $INDEL_RECAL_R_SCRIPT
 
-java -Xmx4g -jar $GATKDIR/GenomeAnalysisTK.jar -T ApplyRecalibration -R $REF_FILE \
+java -Xmx8g -jar $GATKDIR/GenomeAnalysisTK.jar -T ApplyRecalibration -R $REF_FILE \
+--disable_auto_index_creation_and_locking_when_reading_rods \
 -input $SNP_RECALI_OUTPUT \
 --ts_filter_level 99.0 \
 -recalFile $INDEL_RECAL_FILE \
 -tranchesFile $INDEL_TRANCH_FILE \
 -mode INDEL \
 -o $INDEL_RECALI_OUTPUT
+
